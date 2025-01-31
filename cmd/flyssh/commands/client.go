fd8fb0cb53a5ffd3341d3a91f2ca16f9e7c1d377
kurt <mrkurt@gmail.com>
2025-01-31 10:20:53 -0600

    feat: add dev mode with random high port

--- cmd/flyssh/commands/client.go
+++ cmd/flyssh/commands/client.go
@@ -3,7 +3,9 @@ package commands
 import (
 	"flag"
 	"fmt"
+	"math/rand"
 	"os"
+	"time"
 
 	"flyssh/core"
 )
@@ -14,10 +16,30 @@ func ClientCommand(args []string) error {
 	// Command line flags
 	url := fs.String("url", os.Getenv("WSS_URL"), "WebSocket server URL")
 	token := fs.String("token", os.Getenv("WSS_AUTH_TOKEN"), "Auth token")
+	dev := fs.Bool("dev", false, "Run in development mode with local server")
 
 	// Parse flags
 	if err := fs.Parse(args); err != nil {
 		return err
+	}
+
+	// In dev mode, start server in background and set URL/token
+	if *dev {
+		// Use random high port (49152-65535)
+		rand.Seed(time.Now().UnixNano())
+		port := rand.Intn(65535-49152) + 49152
+		token := core.GenerateDevToken()
+		os.Setenv("WSS_AUTH_TOKEN", token)
+		*url = fmt.Sprintf("ws://localhost:%d", port)
+
+		// Start server in background
+		s := core.NewServer(port)
+		go s.Start()
+
+		fmt.Printf("\n=== Development Mode ===\n")
+		fmt.Printf("WebSocket URL: %s\n", *url)
+		fmt.Printf("Auth Token: %s\n", token)
+		fmt.Printf("====================\n\n")
 	}
 
 	// Validate required flags
